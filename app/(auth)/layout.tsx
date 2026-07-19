e26dae6dcce4635152cc8428a819de7cfd46937f
Durodola Abolaji <bolajidurodola@gmail.com>
2026-07-19 18:44:33 +0100

    progress

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-dvh fixed inset-0 flex overflow-hidden items-center justify-center  py-[max(env(safe-area-inset-top,0px),1rem)] pb-[max(env(safe-area-inset-bottom,0px),1rem)]">
+    <div className="min-h-dvh fixed inset-0 flex overflow-hidden items-center justify-center  py-[max(env(safe-area-inset-top,0px),1rem)] ">
       <AuthBackgroundSlideshow />
       {children}
     </div>
--- app/(portal)/layout.tsx
+++ app/(portal)/layout.tsx
@@ -7,11 +7,10 @@ export default function PortalLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex min-h-dvh items-center justify-center px-4  bg-amber-200">
+    <div className="flex min-h-dvh items-center justify-center px-4 py-[max(env(safe-area-inset-top,0px),1rem)] pb-[max(env(safe-area-inset-bottom,0px),1rem)]">
       <SidebarProvider>
         <AppSideBar/>
         <SidebarTrigger />
-        
         {children}
       </SidebarProvider>
     </div>
